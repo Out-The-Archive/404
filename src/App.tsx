bd578d23803e048f0887c8fec6b7962ffd4a2af5
hoz011 <hoz011@ucsd.edu>
2025-04-23 23:16:56 -0700

    add square flower pot block

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ function App() {
                 <div className="flex items-end col-start-2 col-span-9 row-start-6 row-span-2 border border-blue-500">
                     <div className="bg-yellow-900 w-full h-[30px] rounded-sm"></div>
                 </div>
-                <div className="flex items-end col-start-5 col-span-7 row-start-4 row-span-2 border border-blue-500">
+                <div className="flex flex-col justify-end col-start-5 col-span-7 row-start-4 row-span-2 border border-blue-500">
+                    <div className="bg-pink-500 w-[50px] h-[50px] rounded-sm"></div>
                     <div className="bg-yellow-900 w-full h-[30px] rounded-sm"></div>
                 </div>
             </div>
