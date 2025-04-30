2e8b4e7a26819ff1fdc4e60bf698c48b139efff1
Snoopalyt <Celine.yt.leung@gmail.com>
2025-04-30 00:01:28 -0700

    responsive to width change

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,31 @@ function App() {
         <>
             <div className="grid grid-cols-12 grid-rows-8 gap-4 h-screen w-[100vw] border border-red-500">
                 <div className="flex justify-center items-center col-start-11 col-span-2 row-span-2 border border-blue-500">
-                     <div className="bg-yellow-300 w-[70px] h-[70px] rounded-sm"></div>
+                     <div className="bg-yellow-300 w-2/3 aspect-[1/1] rounded-sm"></div>
                 </div>
-                <div className="flex items-end col-start-2 col-span-5 row-start-2 row-span-2 border border-blue-500">
-                    <div className="bg-yellow-900 w-full h-[30px] rounded-sm"></div>
+                <div className="flex flex-col justify-end col-start-2 col-span-5 row-start-2 row-span-2 border border-blue-500">
+                    <div className="flex flex-row justify-around px-4 gap-x-4 overflow-hidden">
+                        <div className="bg-pink-500 basis-1/3 aspect-[1/1] max-w-[250px] rounded-sm"></div>
+                        <div className="bg-pink-500 basis-1/3 aspect-[1/1] max-w-[250px] rounded-sm"></div>
+                        <div className="bg-pink-500 basis-1/3 aspect-[1/1] max-w-[250px] rounded-sm"></div>
+                    </div>  
+                    <div className="bg-yellow-900 w-full h-1/11 flex-shrink-0 rounded-sm"></div>
                 </div>
-                <div className="flex items-end col-start-2 col-span-9 row-start-6 row-span-2 border border-blue-500">
-                    <div className="bg-yellow-900 w-full h-[30px] rounded-sm"></div>
+                <div className="flex flex-col justify-end col-start-2 col-span-9 row-start-6 row-span-2 border border-blue-500">
+                <div className="flex flex-row justify-around px-4 gap-x-4 overflow-hidden">
+                        <div className="bg-pink-500 basis-1/3 aspect-[1/1] max-w-[250px] rounded-sm"></div>
+                        <div className="bg-pink-500 basis-1/3 aspect-[1/1] max-w-[250px] rounded-sm"></div>
+                        <div className="bg-pink-500 basis-1/3 aspect-[1/1] max-w-[250px] rounded-sm"></div>
+                    </div>     
+                    <div className="bg-yellow-900 w-full h-1/11 flex-shrink-0 rounded-sm"></div>
                 </div>
                 <div className="flex flex-col justify-end col-start-5 col-span-7 row-start-4 row-span-2 border border-blue-500">
-                    <div className="flex flex-row justify-between px-4">
-                        <div className="bg-pink-500 w-[100px] h-[100px] rounded-sm"></div>
-                        <div className="bg-pink-500 w-[100px] h-[100px] rounded-sm"></div>
-                        <div className="bg-pink-500 w-[100px] h-[100px] rounded-sm"></div>
+                    <div className="flex flex-row justify-around px-4 gap-x-4 overflow-hidden">
+                        <div className="bg-pink-500 basis-1/3 aspect-[1/1] max-w-[250px] rounded-sm"></div>
+                        <div className="bg-pink-500 basis-1/3 aspect-[1/1] max-w-[250px] rounded-sm"></div>
+                        <div className="bg-pink-500 basis-1/3 aspect-[1/1] max-w-[250px] rounded-sm"></div>
                     </div>     
-                    <div className="bg-yellow-900 w-full h-[30px] rounded-sm"></div>
+                    <div className="bg-yellow-900 w-full h-1/11 flex-shrink-0 rounded-sm"></div>
                 </div>
             </div>
         </>
